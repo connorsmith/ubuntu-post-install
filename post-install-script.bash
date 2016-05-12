1930079692a90039f414690b9f2aa0cb5bf70b2e
Connor Smith <con.smith13@gmail.com>
2016-05-12 17:10:21 +0900

    adds power daemon configurations

--- post-install-script.bash
+++ post-install-script.bash
@@ -28,6 +28,14 @@ mkdir ~/Figures
 # run additional configuration scripts
 bash ./git-settings.bash
 
+# update system settings
+gsettings set com.canonical.indicator.power show-percentage true
+gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://sublime-text.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+gsettings set org.gnome.settings-daemon.plugins.power critical-battery-action 'shutdown'
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'nothing'
+
 # prompt for a reboot
 echo ""
 echo "===================="
