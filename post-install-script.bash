4e37a552bd2b9e0050ffd7ff9acb441290ea83c2
Connor Smith <con.smith13@gmail.com>
2016-05-12 16:34:58 +0900

    adds installation for Sublime Text 3

--- post-install-script.bash
+++ post-install-script.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# adds repos
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+
 # basic update
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
 
 # install apps
-sudo apt-get -y --force-yes install gitk vim
+sudo apt-get -y --force-yes install \
+	gitk vim sublime-text-installer
 
 # autoremove 
 sudo apt-get autoremove
