189f947f1a04ea22ffd4b49f10e8ea81576ccd77
Connor Smith <con.smith13@gmail.com>
2016-05-13 13:51:08 +0900

    moves gsettings to a separate bash script

---
+++ gsettings.bash
@@ -0,0 +1,6 @@
+gsettings set com.canonical.indicator.power show-percentage true
+gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://sublime-text.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+gsettings set org.gnome.settings-daemon.plugins.power critical-battery-action 'shutdown'
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'nothing'
--- post-install-script.bash
+++ post-install-script.bash
@@ -44,14 +44,7 @@ mkdir ~/Figures
 
 # run additional configuration scripts
 bash ./git-settings.bash
-
-# update system settings
-gsettings set com.canonical.indicator.power show-percentage true
-gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://sublime-text.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
-gsettings set org.gnome.settings-daemon.plugins.power critical-battery-action 'shutdown'
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
-gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action 'nothing'
-gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'nothing'
+bash ./gsettings.bash
 
 # changes to bashrc
 echo "export RR_LOCATION=tokyo" >> ~/.bashrc
