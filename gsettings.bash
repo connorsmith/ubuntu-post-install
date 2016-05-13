42320edfbaa1b685cb341cce38b15f8abb5bd017
Connor Smith <con.smith13@gmail.com>
2016-05-13 15:01:46 +0900

    updates gsettings

--- gsettings.bash
+++ gsettings.bash
@@ -1,6 +1,9 @@
 gsettings set com.canonical.indicator.power show-percentage true
-gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://sublime-text.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://sublime-text.desktop', 'application://terminator.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 gsettings set org.gnome.settings-daemon.plugins.power critical-battery-action 'shutdown'
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'nothing'
+gsettings set de.mh21.indicator.multiload.general width '500'
+gsettings set de.mh21.indicator.multiload.general speed '100'
+gsettings set com.canonical.indicator.datetime show-date true
