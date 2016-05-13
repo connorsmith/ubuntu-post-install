79e59cb433bd6a1d63a1aa84097893ec6155ae98
Connor Smith <con.smith13@gmail.com>
2016-05-13 16:03:40 +0900

    adds prereqs for firmware repository

--- post-install-script.bash
+++ post-install-script.bash
@@ -3,6 +3,8 @@
 # adds repos
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo add-apt-repository -y ppa:alexmurray/indicator-sensors
+sudo add-apt-repository -y ppa:terry.guo/gcc-arm-embedded
 
 # ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -11,8 +13,6 @@ sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA
 # gazebo
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-latest.list' 
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-
-sudo add-apt-repository -y ppa:alexmurray/indicator-sensors
 
 # todo: remove 32 chrome: http://www.omgubuntu.co.uk/2016/03/fix-failed-to-fetch-google-chrome-apt-error-ubuntu
 
@@ -23,10 +23,12 @@ sudo apt-get -y --force-yes upgrade
 # install apps
 sudo apt-get -y --force-yes install \
 	gitk vim sublime-text-installer google-chrome-stable vlc \
-	screen terminator ssh python-pip doxygen python-wstool \
-	indicator-multiload  indicator-sensors\
-	ros-indigo-desktop python-rosinstall\
-	ros-indigo-gazebo5-ros-pkgs
+	screen terminator ssh cmake-curses-gui \
+	python-pip doxygen python-wstool python-catkin-pkg python-pkg-resources \
+	indicator-multiload  indicator-sensors \
+	python-rosinstall gcc-arm-none-eabi openocd\
+	ros-indigo-gazebo5-ros-pkgs \
+	ros-indigo-desktop
 
 sudo pip install jinja2
 
